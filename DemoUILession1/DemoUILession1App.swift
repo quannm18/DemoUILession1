0f22968421b72927938ba8ff6fa8b151d0d5f1fb
quannm18 <ngominhquan20021809@gmail.com>
2023-08-27 09:57:09 +0700

    Initial Commit

---
+++ DemoUILession1/DemoUILession1App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoUILession1App.swift
+//  DemoUILession1
+//
+//  Created by Ngô Minh Quân on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoUILession1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
